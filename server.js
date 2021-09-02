7a123f6c4a3292cf2751131ee2b849cdc21b8719
James Crosslin <71192790+jamescrosslin@users.noreply.github.com>
2021-09-02 13:15:14 -0700

    Add console logs & redirect to server.js
    Redirects should handle authorization for your use case. Console logs will let your know if your isAuthorized method is ever returning true for debugging purposes

--- server.js
+++ server.js
@@ -47,8 +47,6 @@ const config = {
     idpLogout: true,
 }
 
-
-
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))//handle nested data coming thru the query string
@@ -56,13 +54,20 @@ app.use(express.json())
 app.use(auth(config))
 
 app.get('/', (req, res) => {
-    res.render('index.ejs', { info: [] })
+    const isAuthenticated = req.oidc.isAuthenticated();
+    console.log("check if user is authenticated: ", isAuthenticated)
+    if(!isAuthenticated){
+        return res.redirect('/login');
+    }
+    res.render('index.ejs', { info: [], isAuthenticated })
 })
 
-
 app.get('/login', (req, res) => {
-    console.log(req.oidc.isAuthenticated())
-    res.render('index.ejs', { isAuthenticated: req.oidc.isAuthenticated() })
+    console.log("check if user is authenticated: ", req.oidc.isAuthenticated())
+    if(req.oidc.isAuthenticated()){
+        return res.redirect('/');
+    }
+    res.render('landingpage.ejs')
 });
 
 
